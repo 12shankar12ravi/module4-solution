d2bdefb38fb0470f193b76d377074b4e31d5c296
12shankar12ravi <12shankar12ravi@gmail.com>
2017-07-16 12:08:59 +0530

    url should use secured protocol https;

--- src/menudata.service.js
+++ src/menudata.service.js
@@ -2,7 +2,7 @@
 
 angular.module ( 'data').
 service('MenuDataService',MenuDataService )
-.constant('myURL',"http://davids-restaurant.herokuapp.com/");
+.constant('myURL',"https://davids-restaurant.herokuapp.com/");
 
 
 MenuDataService.$inject = ['$http' , 'myURL'];
